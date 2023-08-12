8be0e9c5fba85c0bedccf36f21345a6a84f392c3
Ferwin Lopez <ferwinlopezt@gmail.com>
2023-08-12 22:38:41 +0530

    testing localStorage

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 const pickerBtn = document.getElementById("pick")
 const resultDiv  = document.querySelector(".results");
+let hex = [];
 const activateEyeDropper = async () =>{
 
   try{
     const eyeDropper = new EyeDropper();
     const {sRGBHex} = await eyeDropper.open();
     navigator.clipboard.writeText(sRGBHex);
-    let hex = sRGBHex;
-    resultDiv.innerHTML +=hex;
-    console.log(sRGBHex);
-
+    hex.push(sRGBHex);
+    resultDiv.innerHTML +=sRGBHex;
+    localStorage.setItem("picked",hex);
+   
+    
   }
   catch(error){
     console.log(error);
   }
 }
-
+console.log(localStorage.getItem("picked"));
 pickerBtn.addEventListener("click",activateEyeDropper);
