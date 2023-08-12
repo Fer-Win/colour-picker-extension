525203fccf680195ffab67b877335d06f0582e7e
Ferwin Lopez <ferwinlopezt@gmail.com>
2023-08-12 20:56:56 +0530

    Added display function

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const pickerBtn = document.getElementById("pick")
-
+const resultDiv  = document.querySelector(".results");
 const activateEyeDropper = async () =>{
 
   try{
     const eyeDropper = new EyeDropper();
     const {sRGBHex} = await eyeDropper.open();
     navigator.clipboard.writeText(sRGBHex);
+    resultDiv.innerHTML=sRGBHex;
     console.log(sRGBHex);
+
   }
   catch(error){
     console.log(error);
